ed92663475e4f79933d7f22bd1336e6a7eba3b29
Diogo Machado <diogomachado_8@hotmail.com>
2020-11-22 13:31:03 -0300

    fix/env

--- services/api.js
+++ services/api.js
@@ -1,12 +1,7 @@
 import Axios from "axios";
 
-let urls = {
-  test: `http://localhost:3333/`,
-  development: "http://localhost:3333/v1/",
-  production: "http://localhost:3333/v1/",
-};
 const api = Axios.create({
-  baseURL: urls[process.env.NODE_ENV],
+  baseURL: process.env.API_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
